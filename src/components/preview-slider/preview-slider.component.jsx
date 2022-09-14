56b6425e3e45da67ecf70e164e3aaee42aed6080
codingmarisa <codingmarisa@gmail.com>
2022-09-14 01:11:29 -0700

    Slider image styling done

--- src/components/preview-slider/preview-slider.component.jsx
+++ src/components/preview-slider/preview-slider.component.jsx
@@ -4,21 +4,36 @@ import './preview-slider.styles.css';
 function PreviewSlider({ slides }) {
   const [items, setItems] = useState(slides.items.slice(0, 4));
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [activeDot, setActiveDot] = useState();
+
+  const lastSlide = {
+    title: 'View the Full Exhibit',
+    details: 'details go here',
+  };
 
   const previousSlide = () => {
     if (currentIndex === 0) {
       setCurrentIndex(4);
+      setActiveDot(4);
     } else {
       setCurrentIndex(currentIndex - 1);
+      setActiveDot(currentIndex - 1);
     }
   };
 
   const nextSlide = () => {
     if (currentIndex === 4) {
       setCurrentIndex(0);
+      setActiveDot(0);
     } else {
       setCurrentIndex(currentIndex + 1);
+      setActiveDot(currentIndex + 1);
     }
+  };
+
+  const dotClickHandler = (slideIndex) => {
+    goToSlide(slideIndex);
+    setActiveDot(slideIndex);
   };
 
   const goToSlide = (slideIndex) => {
@@ -29,8 +44,9 @@ function PreviewSlider({ slides }) {
     <div className="preview-image-slider-container">
       <div>
         {currentIndex === 4 ? (
-          <div>
-            <p>View the Full Exhibit</p>
+          <div className="preview-last-slide">
+            <p>{lastSlide.title}</p>
+            <p>{lastSlide.details}</p>
           </div>
         ) : (
           <div
@@ -38,7 +54,16 @@ function PreviewSlider({ slides }) {
             style={{
               backgroundImage: `url(${items[currentIndex].image})`,
             }}
-          />
+          >
+            <div className="preview-slider-details">
+              <p>{items[currentIndex].title}</p>
+              {slides.singleArtist ? (
+                <p>{slides.aboutSingleArtist.credit}</p>
+              ) : (
+                <p>{items[currentIndex].credit}</p>
+              )}
+            </div>
+          </div>
         )}
         <div
           className="preview-slider-arrow preview-left-arrow"
@@ -56,15 +81,28 @@ function PreviewSlider({ slides }) {
       <div className="preview-slider-dots-container">
         {items.map((item, itemIndex) => (
           <div
-            className="preview-slider-dot"
+            className={
+              activeDot === itemIndex
+                ? 'preview-slider-dot preview-active-dot'
+                : 'preview-slider-dot'
+            }
+            id={`dot${itemIndex}`}
             key={'slide' + itemIndex}
-            onClick={() => goToSlide(itemIndex)}
+            onClick={() => dotClickHandler(itemIndex)}
           >
             &#9679;
           </div>
         ))}
-        <div className="preview-slider-dot" onClick={() => goToSlide(4)}>
-          &#9673;
+        <div
+          className={
+            activeDot === 4
+              ? 'preview-slider-dot preview-active-dot'
+              : 'preview-slider-dot'
+          }
+          id="dot4"
+          onClick={() => dotClickHandler(4)}
+        >
+          &#9679;
         </div>
       </div>
     </div>
